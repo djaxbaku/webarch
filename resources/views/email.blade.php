ebfd9c7efdcf8e64ac19684cd2fd828b2688c329
djaxbaku <alixalilzade@gmail.com>
2017-02-01 13:53:35 +0400

    dghghfhj

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,7 +1,7 @@
   <!DOCTYPE html>
   <html>
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8"> 
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>Webarch</title>
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
